2be6c528176313ca641b19d594fe54510a226f4f
Sir Code <taylorschneider07@yahoo.com>
2022-10-04 23:01:05 -0400

    Add tons of stuff. Like... Tons of stuff

--- Assets/Scripts/Object.cs
+++ Assets/Scripts/Object.cs
@@ -2,25 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-enum Type
+public enum Type
 {
     Rectangle,
     Circle
 }
 
+public enum Operation
+{
+    Union,
+    Subtraction,
+    Intersection,
+}
+
 public class Object : MonoBehaviour
 {
-    [SerializeField]
-    Type type = new Type();
+    public Color color = Color.white;
+    public Type type = new Type();
+    public Operation operation = new Operation();
+    [HideInInspector]
+    public int children = 0;
 
-    void Start()
-    {
-        
+    public Vector2 Position {
+        get {
+            return transform.position;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public Vector2 Scale {
+        get {
+            Vector2 parentScale = Vector2.one;
+            if (transform.parent != null && transform.parent.GetComponent<Object>() != null) {
+                parentScale = transform.parent.GetComponent<Object>().Scale;
+            }
+            return Vector3.Scale(transform.localScale, parentScale);
+        }
     }
 }
--- Assets/Scripts/Raymarch.cs
+++ Assets/Scripts/Raymarch.cs
@@ -2,6 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+struct ObjectData {
+    public Vector2 position;
+    public Vector2 scale;
+    public Vector3 color;
+    public int type;
+    public int operation;
+    public int children;
+    public static int GetSize() {
+        // 2 Vec2s + 1 Vec3, 3 Ints
+        return sizeof(float) * 7 + sizeof(int) * 3;
+    }
+}
+
 [ExecuteInEditMode, ImageEffectAllowedInSceneView]
 public class Raymarch : MonoBehaviour
 {
@@ -27,14 +40,59 @@ public class Raymarch : MonoBehaviour
         }
     }
 
-    private void OnRenderImage(RenderTexture source, RenderTexture destination) {
-        Init();
-        InitTexture();
+    private void InitScene() {
+        List<Object> objects = new List<Object>(FindObjectsOfType<Object>());
+        objects.Sort((a, b) => a.operation.CompareTo(b.operation));
+        List<Object> ordered = new List<Object>();
+        for (int i = 0; i < objects.Count; i++) {
+            if (objects[i].transform.parent == null) {
+                Transform parent = objects[i].transform;
+                ordered.Add(objects[i]);
+                objects[i].children = parent.childCount;
+                for (int j = 0; j < parent.childCount; j++) {
+                    if (parent.GetChild(j).GetComponent<Object>() != null) {
+                        ordered.Add(parent.GetChild(j).GetComponent<Object>());
+                        ordered[ordered.Count - 1].children = 0;
+                    }
+                }
+            }
+        }
+        ObjectData[] data = new ObjectData[ordered.Count];
+        for (int i = 0; i < ordered.Count; i++) {
+            var s = ordered[i];
+            Vector3 col = new Vector3(s.color.r, s.color.g, s.color.b);
+            data[i] = new ObjectData() {
+                position = s.Position,
+                scale = s.Scale, 
+                color = col,
+                type = (int)s.type,
+                operation = (int)s.operation,
+                children = s.children
+            };
+        }
+        ComputeBuffer sceneBuffer = new ComputeBuffer(data.Length, ObjectData.GetSize());
+        sceneBuffer.SetData(data);
+        computeShader.SetBuffer(0, "objects", sceneBuffer);
+        computeShader.SetInt("objectCount", data.Length);
+        disposables.Add(sceneBuffer);
+    }
+
+    private void RenderData(RenderTexture source, RenderTexture destination) {
         computeShader.SetTexture(0, "MainTexture", renderTexture);
         int tgx = Mathf.CeilToInt(camera.pixelWidth / 8.0f);
         int tgy = Mathf.CeilToInt(camera.pixelHeight / 8.0f);
         computeShader.Dispatch(0, tgx, tgy, 1);
         Graphics.Blit(renderTexture, destination);
+        foreach (var buffer in disposables) {
+            buffer.Dispose();
+        }
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        Init();
+        InitTexture();
+        InitScene();
+        RenderData(source, destination);
     }
 
     // Update is called once per frame
